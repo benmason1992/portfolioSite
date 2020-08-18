7e22a01b135146eedcd708beaf40d65b33f7f179
Ben <benmason1992@hotmail.co.uk>
2020-08-18 22:06:44 +0100

    Updates

---
+++ precache-manifest.e1f3e5aee13082e8736e419b941715af.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "350a732fa4dc8a79ccf84633370c4620",
+    "url": "/portfolioSite/index.html"
+  },
+  {
+    "revision": "917491539b44b983f73b",
+    "url": "/portfolioSite/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "755b56ec66f73aa0f68b",
+    "url": "/portfolioSite/static/css/main.39ae4f65.chunk.css"
+  },
+  {
+    "revision": "917491539b44b983f73b",
+    "url": "/portfolioSite/static/js/2.0b2213d0.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/portfolioSite/static/js/2.0b2213d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "755b56ec66f73aa0f68b",
+    "url": "/portfolioSite/static/js/main.e0dc26ba.chunk.js"
+  },
+  {
+    "revision": "9f209761fceeacdaab18",
+    "url": "/portfolioSite/static/js/runtime-main.978f1483.js"
+  }
+]);
