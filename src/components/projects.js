0ad6a96bbbcc5f47a2abf825d84c2bcb53cc588a
Ben <benmason1992@hotmail.co.uk>
2020-10-06 11:44:45 +0100

    Updated laravel images and removed background colour on main page, left banner only

--- src/components/projects.js
+++ src/components/projects.js
@@ -42,7 +42,7 @@ class Projects extends Component {
             return (
                 <div className="projects-grid">
                     <Card shadow={10} style={{ width: '450', margin: 'auto' }}>
-                        <CardTitle style={{ color: '#000', height: '176px', background: 'url(https://camo.githubusercontent.com/c4b3056564d4d97f40afa08cffefa26c2a695316/68747470733a2f2f7265732e636c6f7564696e6172792e636f6d2f6474666276766b79702f696d6167652f75706c6f61642f76313536363333313337372f6c61726176656c2d6c6f676f6c6f636b75702d636d796b2d7265642e737667) center' }}>MIXR API</CardTitle>
+                        <CardTitle style={{ color: '#000', height: '176px', background: 'url(https://vulpo.io/wp-content/uploads/2018/06/laravel-1.png) center / cover' }}>MIXR API</CardTitle>
                         <CardText>
                             I created the API in Laravel and MySQL for the back-end of this project. With a RESTful resource controller, and eloquent many to many relationship.
                         </CardText>
@@ -55,7 +55,7 @@ class Projects extends Component {
                         </CardMenu>
                     </Card>
                     <Card shadow={10} style={{ width: '450', margin: 'auto' }}>
-                        <CardTitle style={{ color: '#000', height: '176px', background: 'url(https://camo.githubusercontent.com/c4b3056564d4d97f40afa08cffefa26c2a695316/68747470733a2f2f7265732e636c6f7564696e6172792e636f6d2f6474666276766b79702f696d6167652f75706c6f61642f76313536363333313337372f6c61726176656c2d6c6f676f6c6f636b75702d636d796b2d7265642e737667) center' }}>instaBen</CardTitle>
+                        <CardTitle style={{ color: '#000', height: '176px', background: 'url(https://vulpo.io/wp-content/uploads/2018/06/laravel-1.png) center / cover' }}>instaBen</CardTitle>
                         <CardText>
                             Created a basic clone of instagram using MVC that allows users to create their own profiles, add their images, follow other users and see other users uploaded images on a timeline.
                         </CardText>
