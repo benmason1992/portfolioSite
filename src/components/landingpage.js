ab37e56bf3098683f09d6b72aa946760c00c159c
Ben <benmason1992@hotmail.co.uk>
2020-10-20 19:51:24 +0100

    updated profile picture

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -7,7 +7,7 @@ class Landing extends Component {
             <div style= {{width: "100%", height:"100%", margin: "auto"}}>
                 <Grid className="landing-grid">
                     <Cell col={12}>
-                        <img src="https://media-exp1.licdn.com/dms/image/C4E03AQEolgWukYsanQ/profile-displayphoto-shrink_400_400/0?e=1602720000&v=beta&t=nbmt8UussvkmW9yZDExP6m7_BMFzL5z8v2WItI-SKk4" alt="myself" className="image-of-me"/>
+                        <img src="https://media-exp1.licdn.com/dms/image/C4E03AQEolgWukYsanQ/profile-displayphoto-shrink_400_400/0?e=1608768000&v=beta&t=EfLHw-3UsllhFxwYQqbCEnjZUWY-wFM8V_t-OKQ5TBc" alt="myself" className="image-of-me"/>
                         <div className="banner-text">
                             <h1>Full Stack Web Developer</h1>
                             <hr/>
