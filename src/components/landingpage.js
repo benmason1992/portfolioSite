3062cc816c9f0f38241e799fd8c2b410c85739b6
Ben <benmason1992@hotmail.co.uk>
2021-01-03 15:17:30 +0000

    updated image to local

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Grid, Cell } from "react-mdl";
+import me from './image/me.jpg';
 
 class Landing extends Component {
     render() {
@@ -7,7 +8,7 @@ class Landing extends Component {
             <div style= {{width: "100%", height:"100%", margin: "auto"}}>
                 <Grid className="landing-grid">
                     <Cell col={12}>
-                        <img src="https://media-exp1.licdn.com/dms/image/C4E03AQEolgWukYsanQ/profile-displayphoto-shrink_400_400/0?e=1608768000&v=beta&t=EfLHw-3UsllhFxwYQqbCEnjZUWY-wFM8V_t-OKQ5TBc" alt="myself" className="image-of-me"/>
+                        <img src={me} alt="myself" className="image-of-me"/>
                         <div className="banner-text">
                             <h1>Full Stack Web Developer</h1>
                             <hr/>
