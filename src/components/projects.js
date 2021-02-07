cd8fd98062dcec9ed6b5b43609e2dc8f3725d788
Ben <benmason1992@hotmail.co.uk>
2021-02-07 15:50:19 +0000

    made changes to layout with more projects

--- src/components/projects.js
+++ src/components/projects.js
@@ -11,55 +11,59 @@ class Projects extends Component {
     toggleCategories() {
         if (this.state.activeTab === 0) {
             return (
-                <div className="projects-grid">
-                    <Card shadow={10} className="projects-card">
-                        <CardTitle style={{ color: '#000', height: '176px', background: 'url(https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2560px-React-icon.svg.png) center / cover' }}>Portfolio Website</CardTitle>
-                        <CardText>
-                            This very website was created in React, see github below!
+                <div>
+                    <div className="projects-grid">
+                        <Card shadow={10} className="projects-card">
+                            <CardTitle style={{ color: '#000', height: '176px', background: 'url(https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2560px-React-icon.svg.png) center / cover' }}>Portfolio Website</CardTitle>
+                            <CardText>
+                                This very website was created in React, see github below!
                         </CardText>
-                        <CardActions border>
-                            <Button colored><a href="https://github.com/benmason1992/portfolioSite" target="_blank" rel="noopener noreferrer">Github</a></Button>
-                        </CardActions>
-                        <CardMenu style={{ color: '#fff' }}>
-                            <IconButton name="share" />
-                        </CardMenu>
-                    </Card>
-                    <Card shadow={10} className="projects-card">
-                        <CardTitle style={{ color: '#000', height: '176px', background: 'url(https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2560px-React-icon.svg.png) center / cover' }}>Restaurant Menu</CardTitle>
-                        <CardText>
-                            React app to display restaurant menu, can toggle what sections of the menu appear.
+                            <CardActions border>
+                                <Button colored><a href="https://github.com/benmason1992/portfolioSite" target="_blank" rel="noopener noreferrer">Github</a></Button>
+                            </CardActions>
+                            <CardMenu style={{ color: '#fff' }}>
+                                <IconButton name="share" />
+                            </CardMenu>
+                        </Card>
+                        <Card shadow={10} className="projects-card">
+                            <CardTitle style={{ color: '#000', height: '176px', background: 'url(https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2560px-React-icon.svg.png) center / cover' }}>Restaurant Menu</CardTitle>
+                            <CardText>
+                                React app to display restaurant menu, can toggle what sections of the menu appear.
                         </CardText>
-                        <CardActions border>
-                            <Button colored><a href="https://github.com/benmason1992/menuReact" target="_blank" rel="noopener noreferrer">Github</a></Button>
-                        </CardActions>
-                        <CardMenu style={{ color: '#fff' }}>
-                            <IconButton name="share" />
-                        </CardMenu>
-                    </Card>
-                    <Card shadow={10} className="projects-card">
-                        <CardTitle style={{ color: '#000', height: '176px', background: 'url(https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2560px-React-icon.svg.png) center / cover' }}>Table Tennis Bracket App</CardTitle>
-                        <CardText>
-                            This was my final project in March 2020 following my attendence at DevelopMe.
+                            <CardActions border>
+                                <Button colored><a href="https://github.com/benmason1992/menuReact" target="_blank" rel="noopener noreferrer">Github</a></Button>
+                            </CardActions>
+                            <CardMenu style={{ color: '#fff' }}>
+                                <IconButton name="share" />
+                            </CardMenu>
+                        </Card>
+                    </div>
+                    <div className="projects-grid">
+                        <Card shadow={10} className="projects-card">
+                            <CardTitle style={{ color: '#000', height: '176px', background: 'url(https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2560px-React-icon.svg.png) center / cover' }}>Table Tennis Bracket App</CardTitle>
+                            <CardText>
+                                This was my final project in March 2020 following my attendence at DevelopMe.
                         </CardText>
-                        <CardActions border>
-                            <Button colored><a href="https://github.com/benmason1992/tech-challenge" target="_blank" rel="noopener noreferrer">Github</a></Button>
-                        </CardActions>
-                        <CardMenu style={{ color: '#fff' }}>
-                            <IconButton name="share" />
-                        </CardMenu>
-                    </Card>
-                    <Card shadow={10} className="projects-card">
-                        <CardTitle style={{ color: '#000', height: '176px', background: 'url(https://zdnet1.cbsistatic.com/hub/i/2019/04/21/9d791bf2-9b20-476a-bba6-201e622d2a1f/jquery-logo-blue.png) center / cover' }}>GMaps Order App</CardTitle>
-                        <CardText>
-                            An application that allows you to enter orders and have markers placed on google maps.
+                            <CardActions border>
+                                <Button colored><a href="https://github.com/benmason1992/tech-challenge" target="_blank" rel="noopener noreferrer">Github</a></Button>
+                            </CardActions>
+                            <CardMenu style={{ color: '#fff' }}>
+                                <IconButton name="share" />
+                            </CardMenu>
+                        </Card>
+                        <Card shadow={10} className="projects-card">
+                            <CardTitle style={{ color: '#000', height: '176px', background: 'url(https://zdnet1.cbsistatic.com/hub/i/2019/04/21/9d791bf2-9b20-476a-bba6-201e622d2a1f/jquery-logo-blue.png) center / cover' }}>GMaps Order App</CardTitle>
+                            <CardText>
+                                An application that allows you to enter orders and have markers placed on google maps.
                         </CardText>
-                        <CardActions border>
-                            <Button colored><a href="https://github.com/benmason1992/orderGmap" target="_blank" rel="noopener noreferrer">Github</a></Button>
-                        </CardActions>
-                        <CardMenu style={{ color: '#fff' }}>
-                            <IconButton name="share" />
-                        </CardMenu>
-                    </Card>
+                            <CardActions border>
+                                <Button colored><a href="https://github.com/benmason1992/orderGmap" target="_blank" rel="noopener noreferrer">Github</a></Button>
+                            </CardActions>
+                            <CardMenu style={{ color: '#fff' }}>
+                                <IconButton name="share" />
+                            </CardMenu>
+                        </Card>
+                    </div>
                 </div>
             );
         } else if (this.state.activeTab === 1) {
